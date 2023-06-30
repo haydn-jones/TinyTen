4f844da1cef11d889a0a722e33804c0e0935a089
Haydn Jones <haydnjonest@gmail.com>
2023-06-30 09:38:23 -0600

    Testing changes

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -14,14 +14,18 @@ template <typename T> class Tensor {
 
     Tensor() = default;
 
+    /*
+     * @brief Construct a tensor with given dimensions. Data is initialized to zero.
+     * @param dimensions The dimensions of the tensor.
+     */
     Tensor(const IndexType& dimensions) : dimensions_(dimensions) {
         int total_size = std::accumulate(dimensions.begin(), dimensions.end(), 1, std::multiplies<int>());
         data_.resize(total_size);
     }
 
-    ValueType& operator[](const IndexType& indices) { return data_[index(indices)]; }
+    ValueType& operator[](const IndexType& indices) { return data_[flatten_index(indices)]; }
 
-    const ValueType& operator[](const IndexType& indices) const { return data_[index(indices)]; }
+    const ValueType& operator[](const IndexType& indices) const { return data_[flatten_index(indices)]; }
 
     SizeType size() const noexcept { return data_.size(); }
 
@@ -34,12 +38,10 @@ template <typename T> class Tensor {
     ContainerType data_;
     IndexType dimensions_;
 
-    SizeType index(const IndexType& indices) const {
-        assert(indices.size() == dimensions_.size());
+    SizeType flatten_index(const IndexType& indices) const {
         SizeType idx = 0;
         SizeType stride = 1;
         for (int i = indices.size() - 1; i >= 0; --i) {
-            assert(indices[i] < dimensions_[i]);
             idx += stride * indices[i];
             stride *= dimensions_[i];
         }
--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,38 @@
 #include <catch2/catch_test_macros.hpp>
 #include <cstdint>
-
 #include <tensor.hpp>
 
-TEST_CASE("Building Tensor", "tensor" ) {
-    auto ten = Tensor<float>({2, 3, 4});
-    REQUIRE( ten.size() == 24 );
+TEST_CASE("Tensor operations", "[Tensor]") {
+    using TensorType = Tensor<int>;
+    using IndexType = TensorType::IndexType;
+
+    SECTION("default constructed tensor should be empty") {
+        TensorType tensor;
+        REQUIRE(tensor.size() == 0);
+    }
+
+    SECTION("constructed with dimensions should have correct size") {
+        TensorType tensor({4, 4, 4});
+        REQUIRE(tensor.size() == 4 * 4 * 4);
+    }
+
+    SECTION("indexing should work correctly") {
+        TensorType tensor({4, 4, 4});
+        IndexType idx = {1, 2, 3};
+        tensor[idx] = 10;
+        REQUIRE(tensor[idx] == 10);
+    }
+
+    SECTION("iteration should work correctly") {
+        TensorType tensor({2, 2});
+        int value = 0;
+        for (auto& element : tensor) {
+            element = value++;
+        }
+
+        value = 0;
+        for (const auto& element : tensor) {
+            REQUIRE(element == value++);
+        }
+    }
 }
